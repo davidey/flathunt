204d60b66776a32d7f819773fc760d29f2ee7b8c
Davide Petrillo <davide@ustwo.co.uk>
2012-09-10 19:53:50 +0100

    First import

---
+++ crawl.js
@@ -0,0 +1,26 @@
+// Count all of the links from the nodejs build page
+var jsdom = require("jsdom");
+
+jsdom.env("http://www.gumtree.com/flats-and-houses-for-rent-offered/london", [
+  'http://code.jquery.com/jquery-1.5.min.js'
+],
+
+function(errors, window) {
+  $ = window.$;
+  var results = [];
+  var links = $('#search-results ul.ad-listings li');
+  links.each(function () {
+  var entry = {};
+    var $this = $(this);
+    entry.id = $this.find('a').attr('id').match(/([0-9]+)/g)[0];
+    entry.title = $this.find('a span').text();
+    entry.date = $this.find('span.post-date').attr('title');
+    entry.price = $this.find('span.price').text().match(/[0-9]+/g)[0];
+    entry.link = $this.find('a').attr('href');
+    console.log(entry);
+    results.push(entry);
+  });
+
+
+  console.log(results.length);
+});
