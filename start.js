826628c4169df95db01f81e2f0bedaf78493c313
Davide Petrillo <davide@ustwo.co.uk>
2013-08-21 17:05:31 +0100

    Removed unnecessary files

--- start.js
+++ start.js
@@ -1,10 +1,28 @@
 var Settings = require('./config.js');
 var FlatListPageCrawler = require('./flat-list-page-crawler.js');
+var FlatDataManager = require('./flat-data-manager.js');
+
+var flats = [];
 
 var flatListPageCrawler = new FlatListPageCrawler({
 	jsDomOptions: Settings.jsDomOptions
 });
 
-flatListPageCrawler.crawl(Settings.startPage, function (results) {
-	console.log(results);	
-})
+var flatDataManager = new FlatDataManager({}, function() {
+
+	flatListPageCrawler.crawl(Settings.startPage, function (results) {
+		flats = results;	
+
+		flats.forEach(function (elem) {
+			flatDataManager.flatExists(elem.id, function(err, res) {
+				if (res) {
+					console.log("Flat already there!");
+					return;
+				}
+				console.log("Saving flat");
+				flatDataManager.saveFlat(elem);
+			}.bind(this));
+		});
+	})
+
+});
