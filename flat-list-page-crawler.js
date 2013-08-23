6209c7c554b73ec5dbf11c4ea7c6a6d4365869c4
Davide Petrillo <davide@ustwo.co.uk>
2013-08-23 11:32:01 +0100

    Added sanitisation of entries (if no date, will skip the entry)

--- flat-list-page-crawler.js
+++ flat-list-page-crawler.js
@@ -31,13 +31,22 @@ FlatListPageCrawler.prototype.crawlEngine = function crawlEngine(window, callbac
   var items = $('#search-results ul.ad-listings li.hlisting');
   items.each(function () {
     var $this = $(this);
+
     var entry = {};
     entry.id = $this.find('a').attr('id').match(/([0-9]+)/g)[0];
     entry.title = $this.find('a').attr('title');
-    entry.date = new Date($this.find('span.post-date').attr('title'));
+    entry.date = $this.find('div.ad-features span.dtlisted').attr('title');
     entry.price = $this.find('span.price').text().match(/[0-9]+/g)[0];
     entry.link = $this.find('a').attr('href');
-    //console.log(entry);
+
+    console.log('Found flat id ' + entry.id);
+
+    // Sanitize
+    if (!entry.date) {
+      console.log('Skipping ' + entry.id + ', missing date');
+      return;
+    }
+
     results.items.push(entry);
   });
 
