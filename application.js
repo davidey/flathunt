567c475bcfe204b384e97ef6b373e84d0e4f2a42
Davide Petrillo <petr@illodavi.de>
2013-08-26 23:12:56 +0100

    Added support for queued crawling

--- application.js
+++ application.js
@@ -2,13 +2,15 @@ var moment = require('moment');
 var _ = require('underscore');
 
 var FlatListPageCrawler = require('./flat-list-page-crawler.js');
+var FlatDetailsPageCrawler = require('./flat-details-page-crawler.js');
 var FlatDataManager = require('./flat-data-manager.js');
+var CrawlQueue = require('./crawl-queue.js');
 
 var Application = function Application(appProperties, options) {
 	// Default options
 	this.defaultOptions = {
-		parseNextPageBaseDelay: 10000,
-		parseNextPageMargin: 2500
+		baseInterval: 3000,
+		intervalMargin: 1500
 	};
 
 	this.options = _.extend(this.defaultOptions, options);
@@ -18,51 +20,93 @@ var Application = function Application(appProperties, options) {
 	this.flatListPageCrawler = new FlatListPageCrawler({
 		jsDomOptions: this.options.jsDomOptions
 	});
+
+	this.flatDetailsPageCrawler = new FlatDetailsPageCrawler({
+		jsDomOptions: this.options.jsDomOptions
+	});
+
+	this.flatDataManager = new FlatDataManager({});
+
+	this.startDate = null;
+
+	this.crawlQueue = new CrawlQueue({
+		baseInterval: this.options.baseInterval,
+		intervalMargin: this.options.intervalMargin
+	});
 };
 
 Application.prototype.start = function start(startPage) {
-	this.flatDataManager = new FlatDataManager({});
-	this.parse(startPage, function (result) {
-		this.saveItems(result.items);
-	}.bind(this));
+	this.startDate = new Date();
+	this.enqueueFlatList(startPage);
+	this.crawlQueue.run();
 };
 
-Application.prototype.parse = function parse(page, onCompleteCallback) {
-	this.flatListPageCrawler.crawl(page, function (result) {
-		onCompleteCallback(result);
+Application.prototype.onFlatListManagerResult = function onFlatListManagerResult(result) {
+	this.saveItems(result.items);
 
-		var lastItemIndex = result.items.length - 1;
-		var lastItemDate = new Date(result.items[lastItemIndex].date);
-		var oldestFlatFetch = this.appProperties.get('oldestFlatFetch');
+	var lastItemIndex = result.items.length - 1;
+	var lastItemDate = new Date(result.items[lastItemIndex].date);
+	var oldestFlatFetch = this.appProperties.get('oldestFlatFetch');
 
-		console.log('Check last item date ' + lastItemDate + ' against last fetched date ' + oldestFlatFetch);
+	console.log('Check last item date ' + lastItemDate + ' against last fetched date ' + oldestFlatFetch);
 
-		if (moment(oldestFlatFetch).isBefore(moment(lastItemDate))) {
-			// Parse next page
-			console.log('About to parse next page..');
-			setTimeout(function () {
-				this.parse(result.nextPageLink, onCompleteCallback);
-			}.bind(this), this.generateParseNextPageDelay());
-		}
-	}.bind(this));
+	if (moment(oldestFlatFetch).isBefore(moment(lastItemDate))) {
+		this.enqueueFlatList(result.nextPageLink);
+	} else {
+		// Reached the date limit
+		this.endFlatListCrawling();
+	}
+};
+
+Application.prototype.endFlatListCrawling = function endFlatListCrawling() {
+	this.appProperties.set('oldestFlatFetch', this.startDate);
+	this.appProperties.save();
+
+	this.crawlQueue.stop();
 }
 
 Application.prototype.saveItems = function saveItems(items) {
-	items.forEach(function (item) {
+	var countSkipped = 0;
+	var countSaved = 0;
+
+	var countTotal = items.length;
+
+	items.forEach(function (item, index) {
 		this.flatDataManager.flatExists(item.id, function(err, res) {
 			if (res) {
-				console.log('Flat ' + item.id + ' already there!');
-				return;
+				countSkipped++;
+			} else {
+				countSaved++;
+				item.availableDate = this.parseAvailableDate(item.availableDate);
+				this.flatDataManager.saveFlat(item);
 			}
-			console.log('Saving flat ' + item.id);
-			this.flatDataManager.saveFlat(item);
+
+			if (--countTotal === 0) {
+				console.log('Saved ' + countSaved + ' flats, ' + countSkipped + ' skipped');
+			}
 		}.bind(this));
 	}.bind(this));
 }
 
-Application.prototype.generateParseNextPageDelay = function generateParseNextPageDelay() {
-	var result = this.options.parseNextPageBaseDelay + (Math.random() * this.options.parseNextPageMargin * 2 - this.options.parseNextPageMargin);
+Application.prototype.parseAvailableDate = function parseAvailableDate(date) {
+	var result = moment(date, 'DD/MM/YY');
+
+	if (result === null || !result.isValid()) {
+		console.log('Can\'t parse available date ' + date);
+		result = null;
+	} else {
+		result = result.toDate();
+	}
+
 	return result;
+}
+
+Application.prototype.enqueueFlatList = function enqueueFlatList(uri) {
+	this.crawlQueue.enqueue('list', uri, this.flatListPageCrawler, this.onFlatListManagerResult.bind(this))
+}
+
+Application.prototype.enqueueFlatDetails = function enqueueFlatDetails(id,uri) {
+	this.crawlQueue.enqueue(id, uri, this.flatDetailsPageCrawler, this.onFlatDetailsManagerResult.bind(this))
 }
 
 module.exports = Application;
--- spec/application_spec.js
+++ spec/application_spec.js
@@ -4,6 +4,8 @@ var Config = require('../config');
 var Application = require('../application.js');
 var AppPropertiesModel = require('../model.app-properties.js');
 
+var Settings = Config.test
+
 describe('Application', function () {
 	'use strict';
 
@@ -16,21 +18,35 @@ describe('Application', function () {
 		});
 	});
 
-	describe('parse()', function () {
+	describe('start()', function () {
+		it("should run the crawlQueue", function() {
+			application.start(Settings.startPage)
+
+			expect(application.crawlQueue.isRunning).toBeTruthy();
+		});
+	});
+
+	describe('endFlatListCrawling()', function () {
+		beforeEach(function () {
+			application.start(Settings.startPage);
+			application.endFlatListCrawling();
+		});
+
+		it("should stop the crawlQueue", function() {
+			expect(application.crawlQueue.isRunning).toBeFalsy();
+		});
+
+		it("should set the new date into the appPropertiesModel", function() {
+			expect(application.appProperties.get('oldestFlatFetch')).toEqual(application.startDate);
+		});
+	});
+
+	xdescribe('parse()', function () {
 		it("should parse the list of items in the given page", function(done) {
 			application.parse(Config.test.startPage, function (result) {
 				expect(result.items.length).toBe(55);
 				done();
 			});
-		});
-	});
-
-	describe('generateParseNextPageDelay()', function () {
-		it("should return a value between 7500 and 12500", function() {
-			var result = application.generateParseNextPageDelay();
-
-			expect(result >= 7500).toBeTruthy();
-			expect(result <= 12500).toBeTruthy();
 		});
 	});
 });
